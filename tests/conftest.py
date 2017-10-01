155e1ee671c9194e3d7d0999d73a066c24a4dbcf
Nikolay Novik <nickolainovik@gmail.com>
2017-10-02 00:25:51 +0300

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,8 @@
 import asyncio
-import os
 import gc
 import socket
 
 import pytest
-import uvloop
 
 
 @pytest.fixture(scope='session')
@@ -19,8 +17,6 @@ def unused_port():
 def pytest_generate_tests(metafunc):
     if 'loop_type' in metafunc.fixturenames:
         loop_type = ['asyncio', 'uvloop']
-        if os.environ.get('TOKIO') == 'y':
-            loop_type.append('tokio')
         metafunc.parametrize("loop_type", loop_type)
 
 
@@ -29,12 +25,8 @@ def loop(request, loop_type):
     old_loop = asyncio.get_event_loop()
     asyncio.set_event_loop(None)
     if loop_type == 'uvloop':
+        import uvloop
         loop = uvloop.new_event_loop()
-    elif loop_type == 'tokio':
-        import tokio
-        policy = tokio.TokioLoopPolicy()
-        asyncio.set_event_loop_policy(policy)
-        loop = tokio.new_event_loop()
     else:
         loop = asyncio.new_event_loop()
 
