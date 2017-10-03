c37bcb7957b67dc5306086b07c4fedc4683e37b5
Nikolay Novik <nickolainovik@gmail.com>
2017-10-03 22:44:41 +0300

    update imports

--- aiozipkin/__init__.py
+++ aiozipkin/__init__.py
@@ -1,8 +1,15 @@
-__version__ = '0.0.1a0'
-
+from .aiohttp_helpers import setup, get_tracer, middleware_maker
 from .helpers import create_endpoint
 from .sampler import Sampler
 from .tracer import create, Tracer
 
 
-__all__ = ("Tracer", "Sampler", "create", "create_endpoint")
+__version__ = '0.0.1a0'
+__all__ = ("Tracer",
+           "Sampler",
+           "create",
+           "create_endpoint",
+           "setup",
+           "get_tracer",
+           "request_span",
+           "middleware_maker")
