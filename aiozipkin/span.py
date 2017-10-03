16b277d998b55bf52ac4d7c908c57e50c34049b1
Nikolay Novik <nickolainovik@gmail.com>
2017-10-04 00:05:18 +0300

    move spans to separate file and add NoopSpan

---
+++ aiozipkin/span.py
@@ -0,0 +1,89 @@
+from .helpers import Endpoint, make_timestamp
+
+
+class NoopSpan:
+    def __init__(self, context):
+        self._context = context
+
+    @property
+    def context(self):
+        return self._context
+
+    def start(self, ts=None):
+        return self
+
+    def finish(self, ts=None):
+        return self
+
+    def remote_endpoint(self, servce_name, *, ipv4=None, ipv6=None, port=None):
+        return self
+
+    def tag(self, key, value):
+        return self
+
+    def annotate(self, value, ts=None):
+        return self
+
+    def kind(self, span_kind):
+        return self
+
+    def name(self, span_name):
+        return self
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.finish()
+
+
+class Span(NoopSpan):
+    def __init__(self, tracer, context, record):
+        self._context = context
+        self._record = record
+        self._tracer = tracer
+
+    @property
+    def context(self):
+        return self._context
+
+    def start(self, ts=None):
+        ts = make_timestamp(ts)
+        self._record.start(ts)
+        return self
+
+    def finish(self, ts=None):
+        ts = make_timestamp(ts)
+        self._record.finish(ts)
+        self._tracer._send(self._record)
+        return self
+
+    def remote_endpoint(self, servce_name, *, ipv4=None, ipv6=None, port=None):
+        endpoint = Endpoint(servce_name, ipv4, ipv6, port)
+        self._record.remote_endpoint(endpoint)
+        return self
+
+    def tag(self, key, value):
+        self._record.set_tag(key, value)
+        return self
+
+    def annotate(self, value, ts=None):
+        ts = make_timestamp(ts)
+        self._record.annotate(value, ts)
+        return self
+
+    def kind(self, span_kind):
+        self._record.kind(span_kind)
+        return self
+
+    def name(self, span_name):
+        self._record.name(span_name)
+        return self
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.finish()
--- aiozipkin/tracer.py
+++ aiozipkin/tracer.py
@@ -1,6 +1,7 @@
-from .helpers import Endpoint, make_timestamp, TraceContext
+from .helpers import TraceContext
 from .record import Record
 from .sampler import Sampler
+from .span import Span, NoopSpan
 from .transport import Transport
 from .utils import generate_random_64bit_string, generate_random_128bit_string
 
@@ -32,6 +33,8 @@ class Tracer:
         return self.to_span(new_context)
 
     def new_child(self, context):
+        if not context.sampled:
+            return NoopSpan(context)
         new_context = self._next_context(context)
         return self.to_span(new_context)
 
@@ -42,6 +45,9 @@ class Tracer:
         return self.to_span(new_context)
 
     def to_span(self, context):
+        if not context.sampled:
+            return NoopSpan(context)
+
         record = Record(context, self._local_endpoint)
         self._records[context] = record
         return Span(self, context, record)
@@ -69,54 +75,3 @@ class Tracer:
             debug=False,
             shared=False)
         return new_context
-
-
-class Span:
-    def __init__(self, tracer, context, record):
-        self._context = context
-        self._record = record
-        self._tracer = tracer
-
-    @property
-    def context(self):
-        return self._context
-
-    def start(self, ts=None):
-        ts = make_timestamp(ts)
-        self._record.start(ts)
-        return self
-
-    def finish(self, ts=None):
-        ts = make_timestamp(ts)
-        self._record.finish(ts)
-        self._tracer._send(self._record)
-        return self
-
-    def remote_endpoint(self, servce_name, *, ipv4=None, ipv6=None, port=None):
-        endpoint = Endpoint(servce_name, ipv4, ipv6, port)
-        self._record.remote_endpoint(endpoint)
-        return self
-
-    def tag(self, key, value):
-        self._record.set_tag(key, value)
-        return self
-
-    def annotate(self, value, ts=None):
-        ts = make_timestamp(ts)
-        self._record.annotate(value, ts)
-        return self
-
-    def kind(self, span_kind):
-        self._record.kind(span_kind)
-        return self
-
-    def name(self, span_name):
-        self._record.name(span_name)
-        return self
-
-    def __enter__(self):
-        self.start()
-        return self
-
-    def __exit__(self, exception_type, exception_value, traceback):
-        self.finish()
