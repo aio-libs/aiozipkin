0420bd4633d295fe0dd4ceca7641ed60be9c1044
Nikolay Novik <nickolainovik@gmail.com>
2017-10-06 00:35:56 +0300

    make demo async

--- examples/simple.py
+++ examples/simple.py
@@ -1,35 +1,39 @@
+import asyncio
 import aiozipkin
-import time
 
 
-def main():
+async def run():
     zipkin_address = "http://localhost:9411/api/v2/spans"
     endpoint = aiozipkin.create_endpoint(
-        "service_name2", ipv4="127.0.0.1", port=8080)
+        "simple_service", ipv4="127.0.0.1", port=8080)
     tracer = aiozipkin.create(zipkin_address, endpoint)
 
-    with tracer.new_trace() as span:
+    with tracer.new_trace(sampled=True) as span:
         span.name("root_span")
         span.tag("span_type", "root")
         span.kind("CLIENT")
-        span.annotate("start sql")
-        time.sleep(0.1)
+        span.annotate("SELECT * FROM")
+
+        await asyncio.sleep(0.1)
         span.annotate("start end sql")
 
         with tracer.new_child(span.context) as nested_span:
-            nested_span.name("nested_span1")
+            nested_span.name("nested_span_1")
             nested_span.kind("CLIENT")
             nested_span.tag("span_type", "inner1")
-            nested_span.remote_endpoint("service", ipv4="127.0.0.1", port=9001)
-            time.sleep(0.01)
+            nested_span.remote_endpoint("remote_service_1")
+            await asyncio.sleep(0.01)
 
         with tracer.new_child(span.context) as nested_span:
-            nested_span.name("nested_span2")
+            nested_span.name("nested_span_2")
             nested_span.kind("CLIENT")
-            nested_span.remote_endpoint("service", ipv4="127.0.0.1", port=9002)
+            nested_span.remote_endpoint("remote_service_2")
             nested_span.tag("span_type", "inner2")
-            time.sleep(0.01)
+            await asyncio.sleep(0.01)
 
+        await asyncio.sleep(30)
+        await tracer.close()
 
 if __name__ == "__main__":
-    main()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
