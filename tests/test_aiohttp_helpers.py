1d043bb28a92af3ec60b1249d10672a5526ed5f5
Bruno Bonfils <asyd@asyd.net>
2017-11-27 15:43:31 +0100

    Trying to add tests for remote_endpoint

--- tests/test_aiohttp_helpers.py
+++ tests/test_aiohttp_helpers.py
@@ -2,6 +2,7 @@ import aiozipkin as az
 from aiohttp import web
 from aiohttp.test_utils import make_mocked_request
 from aiozipkin.aiohttp_helpers import middleware_maker
+from unittest import mock
 
 import pytest
 
@@ -20,9 +21,13 @@ async def test_middleware(tracer, fake_transport):
     app = web.Application()
     az.setup(app, tracer)
 
+    # Fake transport, with IPv4
+    transp = mock.Mock()
+    transp.get_extra_info.return_value = ('127.0.0.1', '0')
+   
     async def handler(request):
         return web.Response(body=b'data')
-    req = make_mocked_request('GET', '/', headers={'token': 'x'})
+    req = make_mocked_request('GET', '/', headers={'token': 'x'}, transport=transp)
 
     middleware_factory = middleware_maker()
 
@@ -30,7 +35,16 @@ async def test_middleware(tracer, fake_transport):
     await middleware(req)
     span = az.request_span(req)
     assert span
+    assert span._record._remote_endpoint['ipv4'] == '127.0.0.1'
     assert len(fake_transport.records) == 1
+
+    # Same with IPv6
+    transp.get_extra_info.return_value = ('::1', '0')
+    req = make_mocked_request('GET', '/', headers={'token': 'x'}, transport=transp)
+    middleware = await middleware_factory(app, handler)
+    await middleware(req)
+    span = az.request_span(req)
+    assert span._record._remote_endpoint['ipv6'] == '::1'
 
     # noop span does not produce records
     headers = {'X-B3-Sampled': '0'}
@@ -38,4 +52,4 @@ async def test_middleware(tracer, fake_transport):
     await middleware(req_noop)
     span = az.request_span(req_noop)
     assert span
-    assert len(fake_transport.records) == 1
+    assert len(fake_transport.records) == 2
