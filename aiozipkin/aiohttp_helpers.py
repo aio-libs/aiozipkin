82127f14d53f09030ee6dcdec90fdd46199f591d
Nikolay Novik <nickolainovik@gmail.com>
2017-12-06 22:27:41 +0200

    update parameters to fit to new aiohttp

--- aiozipkin/aiohttp_helpers.py
+++ aiozipkin/aiohttp_helpers.py
@@ -94,7 +94,8 @@ def request_span(request, request_key=REQUEST_AIOZIPKIN_KEY):
     return request[request_key]
 
 
-def _has_span(trace_request_ctx):
+def _has_span(trace_config_ctx):
+    trace_request_ctx = trace_config_ctx.trace_request_ctx
     has_span = (isinstance(trace_request_ctx, dict) and
                 'span_context' in trace_request_ctx)
     return has_span
@@ -104,11 +105,11 @@ def make_trace_config(tracer):
     trace_config = aiohttp.TraceConfig()
 
     async def on_request_start(session, trace_config_ctx, method, url,
-                               headers, trace_request_ctx=None):
-        if not _has_span(trace_request_ctx):
+                               headers):
+        if not _has_span(trace_config_ctx):
             return
 
-        span_context = trace_request_ctx['span_context']
+        span_context = trace_config_ctx.trace_request_ctx['span_context']
         span = tracer.new_child(span_context)
         trace_config_ctx._span = span
         span.start()
@@ -119,9 +120,9 @@ def make_trace_config(tracer):
         span_headers = span.context.make_headers()
         headers.update(span_headers)
 
-    async def on_request_end(session, trace_config_ctx, method, url,
-                             headers, resp, trace_request_ctx=None):
-        if not _has_span(trace_request_ctx):
+    async def on_request_end(session, trace_config_ctx, method, url, headers,
+                             resp):
+        if not _has_span(trace_config_ctx):
             return
 
         span = trace_config_ctx._span
@@ -129,8 +130,8 @@ def make_trace_config(tracer):
         delattr(trace_config_ctx, '_span')
 
     async def on_request_exception(session, trace_config_ctx, method, url,
-                                   headers, error, trace_request_ctx=None):
-        if not _has_span(trace_request_ctx):
+                                   headers, error):
+        if not _has_span(trace_config_ctx):
             return
         span = trace_config_ctx._span
         span.finish(exception=error)
