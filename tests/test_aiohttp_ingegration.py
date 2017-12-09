0307a193656462c4f9e5698e683d5c6df36d0977
Nikolay Novik <nickolainovik@gmail.com>
2017-12-10 00:59:33 +0200

    update test case

--- tests/test_aiohttp_ingegration.py
+++ tests/test_aiohttp_ingegration.py
@@ -91,13 +91,21 @@ async def test_client_signals(tracer, fake_transport):
         await resp.text()
         assert resp.status == 200
 
+        # do not propagate headers
+        ctx = {'span_context': span.context}
+        resp = await session.get(url, trace_request_ctx=ctx)
+        await resp.text()
+        assert resp.status == 200
+
     await session.close()
 
-    assert len(fake_transport.records) == 2
+    assert len(fake_transport.records) == 3
     record1 = fake_transport.records[0].asdict()
     record2 = fake_transport.records[1].asdict()
-    assert record1['parentId'] == record2['id']
-    assert record2['name'] == 'client:signals'
+    record3 = fake_transport.records[2].asdict()
+    assert record2['parentId'] == record3['id']
+    assert record1['parentId'] == record3['id']
+    assert record3['name'] == 'client:signals'
 
 
 @pytest.mark.asyncio
