be97093212d6db634de9d33ab8e31466ef00bd65
Nikolay Novik <nickolainovik@gmail.com>
2018-03-03 20:15:25 +0200

    add aiohttp-theme

--- docs/conf.py
+++ docs/conf.py
@@ -36,8 +36,8 @@ with codecs.open(_version_path, 'r', 'latin1') as fp:
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('..'))
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- General configuration ------------------------------------------------
@@ -53,7 +53,6 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.intersphinx',
     'sphinxcontrib.asyncio',
-#    'sphinxcontrib.blockdiag',
 ]
 
 
