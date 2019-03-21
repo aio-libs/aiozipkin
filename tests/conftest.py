114d0787c02f1f75a5daf9503f62d8d23e5d6f88
Konstantin Stepanov <stepanov.kos@gmail.com>
2019-03-21 18:40:16 +0300

    add retries and batches to transport (#204)

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import asyncio
 import gc
 
 import aiohttp
+from aiohttp import web
 from aiozipkin.helpers import create_endpoint, TraceContext
 from aiozipkin.sampler import Sampler
 from aiozipkin.tracer import Tracer
@@ -9,6 +10,7 @@ from aiozipkin.transport import StubTransport
 from async_generator import yield_, async_generator
 
 import pytest
+from aiohttp.test_utils import TestServer
 
 
 @pytest.fixture(scope='session')
@@ -56,6 +58,66 @@ def context():
 async def client(loop):
     async with aiohttp.ClientSession(loop=loop) as client:
         await yield_(client)
+
+
+class FakeZipkin:
+
+    def __init__(self, loop):
+        self.next_errors = []
+        self.app = web.Application()
+        self.app.router.add_post('/api/v2/spans', self.spans_handler)
+        self.port = None
+        self._loop = loop
+        self._received_data = []
+        self._wait_count = None
+        self._wait_fut = None
+
+    @property
+    def url(self):
+        return 'http://127.0.0.1:%s/api/v2/spans' % self.port
+
+    async def spans_handler(self, request: web.Request) -> web.Response:
+        if len(self.next_errors) > 0:
+            err = self.next_errors.pop(0)
+            if err == 'disconnect':
+                request.transport.close()
+                await asyncio.sleep(1, loop=self._loop)
+            elif err == 'timeout':
+                await asyncio.sleep(60, loop=self._loop)
+            return web.HTTPInternalServerError()
+
+        data = await request.json()
+        if self._wait_count is not None:
+            self._wait_count -= 1
+        self._received_data.append(data)
+        if self._wait_fut is not None and self._wait_count == 0:
+            self._wait_fut.set_result(None)
+
+        return aiohttp.web.Response(text='', status=200)
+
+    def get_received_data(self) -> list:
+        data = self._received_data
+        self._received_data = []
+        return data
+
+    def wait_data(self, count):
+        self._wait_fut = asyncio.Future(loop=self._loop)
+        self._wait_count = count
+        return self._wait_fut
+
+
+@pytest.fixture
+@async_generator
+async def fake_zipkin(loop):
+    zipkin = FakeZipkin(loop=loop)
+
+    server = TestServer(zipkin.app, loop=loop)
+    await server.start_server()
+    zipkin.port = server.port
+
+    await yield_(zipkin)
+
+    await server.close()
 
 
 pytest_plugins = ['docker_fixtures']
---
+++ tests/test_transport.py
@@ -0,0 +1,127 @@
+import asyncio
+from aiohttp.client import ClientTimeout
+
+import pytest
+import aiozipkin as az
+import aiozipkin.transport as azt
+
+
+@pytest.mark.asyncio
+async def test_retry(fake_zipkin, loop):
+    endpoint = az.create_endpoint('simple_service', ipv4='127.0.0.1', port=80)
+
+    tr = azt.Transport(fake_zipkin.url,
+                       send_interval=0.01,
+                       send_max_size=100,
+                       send_attempt_count=3,
+                       send_timeout=ClientTimeout(total=1))
+
+    fake_zipkin.next_errors.append('disconnect')
+    fake_zipkin.next_errors.append('timeout')
+    waiter = fake_zipkin.wait_data(1)
+
+    tracer = await az.create_custom(endpoint, tr)
+
+    with tracer.new_trace(sampled=True) as span:
+        span.name('root_span')
+        span.kind(az.CLIENT)
+
+    await waiter
+    await tracer.close()
+
+    data = fake_zipkin.get_received_data()
+    trace_id = span.context.trace_id
+    assert any(s['traceId'] == trace_id for trace in data for s in trace), data
+
+
+@pytest.mark.asyncio
+async def test_batches(fake_zipkin, loop):
+    endpoint = az.create_endpoint('simple_service', ipv4='127.0.0.1', port=80)
+
+    tr = azt.Transport(fake_zipkin.url,
+                       send_interval=0.01,
+                       send_max_size=2,
+                       send_timeout=ClientTimeout(total=1))
+
+    tracer = await az.create_custom(endpoint, tr)
+
+    with tracer.new_trace(sampled=True) as span:
+        span.name('root_span')
+        span.kind(az.CLIENT)
+        with span.new_child('child_1', az.CLIENT):
+            pass
+        with span.new_child('child_2', az.CLIENT):
+            pass
+
+    # close forced sending data to server regardless of send interval
+    await tracer.close()
+
+    data = fake_zipkin.get_received_data()
+    trace_id = span.context.trace_id
+    assert len(data[0]) == 2
+    assert len(data[1]) == 1
+    assert data[0][0]['name'] == 'child_1'
+    assert data[0][1]['name'] == 'child_2'
+    assert data[1][0]['name'] == 'root_span'
+    assert any(s['traceId'] == trace_id for trace in data for s in trace), data
+
+
+@pytest.mark.asyncio
+async def test_send_full_batch(fake_zipkin, loop):
+    endpoint = az.create_endpoint('simple_service', ipv4='127.0.0.1', port=80)
+
+    tr = azt.Transport(fake_zipkin.url,
+                       send_interval=60,
+                       send_max_size=2,
+                       send_timeout=ClientTimeout(total=1))
+
+    tracer = await az.create_custom(endpoint, tr)
+    waiter = fake_zipkin.wait_data(1)
+
+    with tracer.new_trace(sampled=True) as span:
+        span.name('root_span')
+        span.kind(az.CLIENT)
+
+    await asyncio.sleep(1, loop=loop)
+
+    data = fake_zipkin.get_received_data()
+    assert len(data) == 0
+
+    with tracer.new_trace(sampled=True) as span:
+        span.name('root_span')
+        span.kind(az.CLIENT)
+
+    # batch is full here
+    await waiter
+    data = fake_zipkin.get_received_data()
+    assert len(data) == 1
+
+    # close forced sending data to server regardless of send interval
+    await tracer.close()
+
+
+@pytest.mark.asyncio
+async def test_lost_spans(fake_zipkin, loop):
+    endpoint = az.create_endpoint('simple_service', ipv4='127.0.0.1', port=80)
+
+    tr = azt.Transport(fake_zipkin.url,
+                       send_interval=0.01,
+                       send_max_size=100,
+                       send_attempt_count=2,
+                       send_timeout=ClientTimeout(total=1))
+
+    fake_zipkin.next_errors.append('disconnect')
+    fake_zipkin.next_errors.append('disconnect')
+
+    tracer = await az.create_custom(endpoint, tr)
+
+    with tracer.new_trace(sampled=True) as span:
+        span.name('root_span')
+        span.kind(az.CLIENT)
+
+    await asyncio.sleep(1, loop=loop)
+
+    await tracer.close()
+
+    data = fake_zipkin.get_received_data()
+    assert len(data) == 0
