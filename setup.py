e379fb7278c0d70e0feb851322e19dcb0a0b0707
Nikolay Novik <nickolainovik@gmail.com>
2019-10-08 22:53:48 -0400

    Do not run python3.5 tests

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ classifiers = [
     'License :: OSI Approved :: MIT License',
     'Intended Audience :: Developers',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Operating System :: POSIX',
