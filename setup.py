04f45c956da2d89815acddc1dc13a0c4d36a20ac
Nikolay Novik <nickolainovik@gmail.com>
2019-11-10 19:48:48 -0500

    Add python3.8 build

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ classifiers = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'Operating System :: POSIX',
     'Development Status :: 3 - Alpha',
     'Framework :: AsyncIO',
