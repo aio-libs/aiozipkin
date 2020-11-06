b656c28a3465a0b4a8851e1e1db6a46d5c1f7890
Andrew Svetlov <andrew.svetlov@gmail.com>
2020-11-06 12:24:01 +0200

    Update trove classifiers

--- setup.py
+++ setup.py
@@ -35,8 +35,10 @@ classifiers = [
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
     "Operating System :: POSIX",
-    "Development Status :: 3 - Alpha",
+    "Development Status :: 5 - Production/Stable",
     "Framework :: AsyncIO",
 ]
 
